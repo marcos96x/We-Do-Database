9c1631030fc0267e75e6487cf96968717f15a69e
Marcos Alexandre Rodrigues de Carvalho <45478002+mencholipa14@users.noreply.github.com>
2019-11-22 20:03:24 -0300

    Add files via upload

--- inserts_weDo.sql
+++ inserts_weDo.sql
@@ -1,10 +1,10 @@
 -- Insert de tecnologias 
 insert into tb_tecnologia  values 
-  (default, 'Aplicações Web', 'Engloba todas as tecnologias de desenvolvimento de sistemas web e sites'),
-  (default, 'Aplicações Desktop', 'Engloba todas as tecnologias de desenvolvimento de sistemas desktop'),
-  (default, 'Aplicações Mobile', 'Engloba todas as tecnologias de desenvolvimento de sistemas mobile (aplicativos de celular)'),
-  (default, 'Aplicações Embarcado (IOT)', 'Engloba todas as tecnologias de desenvolvimento de sistemas IOT'),
-  (default, 'Aplicações com arduíno', 'Desenvolvimento voltado para a microcontroladora chamada arduino.'),
+  (default, 'Desenvolvimento Web', 'Engloba todas as tecnologias de desenvolvimento de sistemas web e sites'),
+  (default, 'Desenvolvimento Desktop', 'Engloba todas as tecnologias de desenvolvimento de sistemas desktop'),
+  (default, 'Desenvolvimento Mobile', 'Engloba todas as tecnologias de desenvolvimento de sistemas mobile (aplicativos de celular)'),
+  (default, 'Desenvolvimento Embarcado (IOT)', 'Engloba todas as tecnologias de desenvolvimento de sistemas IOT'),
+  (default, 'Desenvolvimento com Arduíno', 'Desenvolvimento voltado para a microcontroladora chamada arduino.'),
   (default, 'ASP.NET', 'ASP.NET é a plataforma da Microsoft para o desenvolvimento de aplicações Web e é o sucessor da tecnologia ASP.'),
   (default, 'Angular', 'Framework de Javascript para desenvolver SPA'),
   (default, 'Assembly','Assembly é uma notação legível por humanos para o código de máquina que uma arquitetura de computador específica usa, utilizada para programar códigos entendidos por dispositivos computacionais.'),
@@ -38,6 +38,7 @@ insert into tb_tecnologia  values
   (default, 'Python','Python é uma linguagem de programação de alto nível, interpretada, de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.'),
   (default, 'Q#', 'Q# (pronuncia-se como Q sharp) é uma linguagem de programação de domínio específico usada para expressar algoritmos quânticos. Ele foi inicialmente lançado para o público pela Microsoft como parte do Kit de Desenvolvimento Quantum'),
   (default, 'R', 'R é um ambiente computacional e uma linguagem de programação que vem progressivamente se especializando em manipulação, análise e visualização gráfica de dados'),
+  (default, 'React Native', 'Framework de Javascript para desenvolvimento híbrido em mobile'),
   (default, 'Scala','Scala é uma linguagem de programação de propósito geral, projetada para expressar padrões de programação comuns de uma forma concisa, elegante e type-safe.'),
   (default, 'Shellscript', 'Linguagem de scripts em baixo nível'),
   (default, 'SQL','Structured Query Language, ou Linguagem de Consulta Estruturada ou SQL, é a linguagem de pesquisa declarativa padrão para banco de dados relacional. Muitas das características originais do SQL foram inspiradas na álgebra relacional.'),
@@ -45,7 +46,8 @@ insert into tb_tecnologia  values
   (default, 'TypeScript','TypeScript é um superconjunto de JavaScript desenvolvido pela Microsoft que adiciona tipagem e alguns outros recursos a linguagem.'),
   (default, 'UML','A UML - Linguagem de Modelagem Unificada é uma linguagem-padrão para a elaboração da estrutura de projetos de software.'),
   (default, 'Unreal Engine','Unreal Engine é um motor de jogo desenvolvido pela Epic Games, usado pela primeira vez em 1998 no jogo de tiro em primeira pessoa Unreal, ele tem sido a base de muitos jogos desde então.'),
-  (default, 'Visual Basic','O Visual Basic é uma linguagem de programação produzida pela empresa Microsoft, e é parte integrante do pacote Microsoft Visual Studio. Sua versão mais recente faz parte do pacote Visual Studio .NET, voltada para aplicações .Net.');
+  (default, 'Visual Basic','O Visual Basic é uma linguagem de programação produzida pela empresa Microsoft, e é parte integrante do pacote Microsoft Visual Studio. Sua versão mais recente faz parte do pacote Visual Studio .NET, voltada para aplicações .Net.'),
+  (default, 'Vue JS', 'Framework Javascript para desenvolvimento web de maneira perfomática');
 
 -- Insert de usuarios 
 insert into tb_usuario values 
@@ -61,32 +63,71 @@ insert into tb_usuario values
   (default, 'osvaldoantoniopietrodias_@gmail.com','EBP32SyYzo','Osvaldo Antonio Pietro Dias','2000-08-25','Densevolvedor Java Junior procurando projetos para se aperfeiçoar.','69937692122', curdate(), 1);
 
 insert into tecnologia_usuario values 
-  (8,1),
-  (13,1),
-  (13,2),
-  (3,2),
-  (4,2),
-  (13,2),
-  (7,2),
-  (11,2),
-  (9,3),
-  (20,3),
-  (8,4),
-  (13,4),
-  (14,4),
-  (18,4),
-  (2,5),
-  (3,5),
-  (4,5),
-  (8,6),
-  (8,7),
-  (13,7),
-  (14,7),
-  (8,8),
-  (17,9),
-  (11,9),
-  (7,10),
-  (9,10);
+  (1, 1),
+  (5, 1),
+  (7, 1),
+  (12, 1),
+  (15, 1),
+  (16, 1),
+  (19, 1),
+  (1, 2),
+  (3, 2),
+  (4, 2),
+  (9, 2),
+  (14, 2),
+  (19, 2),
+  (3, 3),
+  (4, 3),
+  (7, 3),
+  (11, 3),
+  (17, 3),
+  (22, 3),
+  (27, 3),
+  (2, 4),
+  (5, 4),
+  (6, 4),
+  (10, 4),
+  (11, 4),
+  (22, 4),
+  (5, 5),
+  (2, 5),
+  (9, 5),
+  (10, 5),
+  (11, 5),
+  (19, 5),
+  (23, 5),
+  (1, 6),
+  (2, 6),
+  (3, 6),
+  (5, 6),
+  (8, 6),
+  (14, 6),
+  (17, 6),
+  (19, 6),
+  (2, 7),
+  (21, 7),
+  (24, 7),
+  (25, 7),
+  (26, 7),
+  (20, 7),
+  (16, 7),
+  (13, 7),
+  (1, 8),
+  (3, 8),
+  (5, 8),
+  (7, 8),
+  (17, 8),
+  (1, 9),
+  (2, 9),
+  (3, 9),
+  (7, 9),
+  (10, 9),
+  (11, 9),
+  (1, 10),
+  (10, 10),
+  (13, 10),
+  (19, 10);
+  
 
 insert into tb_ideia values 
   (default, 'Sistema de Gerenciamento Escolar (SGE)','Um sistema web/mobile para gerenciar uma escola ensino medio, lançar notas, mostrar faltas e etc', curdate(), 0),
@@ -94,69 +135,122 @@ insert into tb_ideia values
   (default, 'Manoteras','Site para aluguel de ratoeiras e outros produtos relacionados a controle de pragas',curdate(), 0),
   (default, 'Graphite','Sistema para ter acesso a arquivos de vazamentos de dados relacionados a invasão',curdate(), 0),
   (default, 'Poseidon','App para listar praias que estão contaminadas, com afinidade de alertar os banhistas a sair de uma área perigosa',curdate(), 0),
-  (default, 'Zeal','Programa/App para ajudar o trabalhador do campo a ter noções metereológicas melhores e mais precisas.',curdate(), 0);
+  (default, 'Zeal','Programa/App para ajudar o trabalhador do campo a ter noções metereológicas melhores e mais precisas.',curdate(), 0),
+  (default, 'Deen', 'Aplicativo que conversa com pessoas solitárias com temas de gosto do usuário.', curdate(), 0);
 
 insert into tb_tag_ideia values 
   (default, "gerenciamento", 1),
   (default, "escolar", 1),
   (default, "escola", 1),
+  (default, "sistema", 1),
   
   (default, "saude", 2),
   (default, "widgets", 2),
   (default, "ios", 2),
+  (default, "projeto", 2),
+  (default, "vital", 2),
   
   (default, "controle", 3),
   (default, "pragas", 3),
   (default, "ratoeiras", 3),
+  (default, "manoteras", 3),
   
   (default, "vazamento", 4),
   (default, "dados", 4),
   (default, "invasao", 4),
+  (default, "graphite", 4),
   
   (default, "praias", 5),
   (default, "contaminacao", 5),
   (default, "banhistas", 5),
+  (default, "poseidon", 5),
   
   (default, "trabalhador", 6),
   (default, "campo", 6),
-  (default, "meteorologia", 6);
+  (default, "meteorologia", 6),
+  (default, "zeal", 6),
+  
+  (default, "deen", 7),
+  (default, "aplicativo", 7),
+  (default, "conversa", 7),
+  (default, "chat", 7);
 
 insert into tecnologia_ideia values 
-  (8,1),
-  (13,1),
-  (16,1),
-  (17,2),
-  (11,2),
-  (8,3),
-  (13,3),
-  (14,3),
-  (9,4),
-  (14,5),
-  (17,5),
-  (18,5),
-  (19,5);
+  (1, 1),
+  (3, 1),
+  (20, 1),
+  (32, 1),
+  (35, 1),
+
+  (3, 2),
+  (25, 2),
+  (42, 2),
+  (43, 2),
+
+  (1, 3),
+  (12, 3),
+  (13, 3),
+  (1, 3),
+  (24, 3),
+  (25, 3),
+  (27, 3),
+  (32, 3),
+  (35, 3),
+
+  (1, 4),
+  (2, 4),
+  (24, 4),
+  (12, 4),
+  (27, 4),
+  (27, 4),
+  (41, 4),
+
+  (39, 5),
+  (3, 5),
+
+  (1, 6),
+  (3, 6),
+  (13, 6),
+  (16, 6),
+
+  (3, 7),
+  (13, 7);
             
 -- insert participante ideia
 -- Status participante  
 -- 0 - Em Aprovação 
 -- 1 - Aprovado
--- 2 - Recusada 
 insert into participante_ideia values 
-  (default, 4,6,1, now(), 1),
-  (default, 9,1,1, now(), 1),
-  (default, 3,2,1, now(), 1),
-  (default, 8,3,1, now(), 1),
-  (default, 6,4,1, now(), 1),
-  (default, 1,5,1, now(), 1),
-  (default, 1,3,1, now(), 0),
-  (default, 2,3,0, now(), 0),
-  (default, 5,3,0, now(), 0),
-  (default, 5,2,1, now(), 0),
-  (default, 6,2,1, now(), 0),
-  (default, 7,1,1, now(), 0),
-  (default, 6,3,1, now(), 0),
-  (default, 10,2,2, now(), 0),
-  (default, 10,2,2, now(), 0);
+	(default, 1,1,1, now(), 1),
+    (default, 3,1,1, now(), 0),
+    (default, 7,1,1, now(), 0),
+    
+    (default, 2,2,1, now(), 1),
+    (default, 6,2,1, now(), 0),
+    (default, 8,2,1, now(), 0),
+    (default, 5,2,1, now(), 0),
+    
+    (default, 3,3,1, now(), 1),
+    (default, 4,3,1, now(), 0),
+    (default, 3,3,1, now(), 0),
+    (default, 1,3,1, now(), 0),
+    (default, 10,3,1, now(), 0),
+    
+    (default, 4,4,1, now(), 1),
+    (default, 2,4,1, now(), 0),
+    (default, 9,4,1, now(), 0),
+    
+    (default, 5,5,1, now(), 1),
+    (default, 10,5,1, now(), 0),
+    
+    (default, 8,6,1, now(), 1),
+    (default, 9,6,1, now(), 0),
+    (default, 1,6,1, now(), 0),
+    (default, 10,6,1, now(), 0),
+    
+    (default, 7,7,1, now(), 1),
+    (default, 3,7,1, now(), 0),
+    (default, 6,7,1, now(), 0);
             
 -- Insert curtida
 insert into curtida_ideia (id_ideia, id_usuario, hr_curtida) values 
@@ -201,3 +295,4 @@ insert into tb_mensagem values
             
             
 insert into tb_adm values (1, 'admin@admin', 'admin123', 'Administrador 1');
+
